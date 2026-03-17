92f14047ed997cad1a2a7468028d6b47e24b4672
Oscar Ruiz <oruizcuadrado@safareyes.es>
2026-03-17 11:44:42 +0100

    Actualizado HelloWorld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo, somos Pajuelo y Gonzalo!");
+        System.out.println("Hola Mundo, somos Pajuelo, Oscar y Gonzalo!");
     }
 }
